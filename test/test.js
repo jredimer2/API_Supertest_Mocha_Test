f94a33863de744815310d409f019eca96cc9af05
Joseph <jredimer@yahoo.com.au>
2022-09-18 02:05:28 +1000

    Code cleanup

--- test/test.js
+++ test/test.js
@@ -7,16 +7,19 @@ const host = 'https://petstore.swagger.io/v2';
 describe('Swagger Pet Store API Tests', function () {
 
     it('Find pet by Id', async function () {
-        const id = '500';
-        const response = await axios.get(host + `/pet/${id}`);
-        expect(response.data).to.include({ id: 500 })
+        const id = 500;
+        await axios.get(host + `/pet/${id}`).then(response => {
+            expect(response.data).to.include({ id })
+        })
     }
     ).timeout(10000)
+
 
     it('Update pet name', async function () {
         const id = 500;
         const name = 'goofy';
-        const updateResponse = await axios.put(host + `/pet`, {
+        // step 1: update pet data
+        await axios.put(host + `/pet`, {
             id,
             name: name,
             photoUrls: [
@@ -30,11 +33,15 @@ describe('Swagger Pet Store API Tests', function () {
             ],
             status: "pending"
         });
-        const getResponse = await axios.get(host + `/pet/${id}`);
-        expect(getResponse.data).to.include({ id })
-        expect(getResponse.data).to.include({ name })
+
+        // step 2: verify data has been updated
+        const getResponse = await axios.get(host + `/pet/${id}`).then(response => {
+            expect(response.data).to.include({ id })
+            expect(response.data).to.include({ name })
+        });
     }
     ).timeout(10000)
+
 
     it('Delete a pet', async function () {
         // step 1: create a new pet, to be deleted in next step
@@ -56,22 +63,23 @@ describe('Swagger Pet Store API Tests', function () {
         });
 
         // step 2: delete pet
-        const deleteResponse = await axios.delete(host + `/pet/${id}`)
+        await axios.delete(host + `/pet/${id}`);
 
         // step 3: verify if pet has been deleted
         try {
-            var getResponse = await axios.get(host + `/pet/${id}`);
-            expect(getResponse.data).to.not.include({ id })     
+            await axios.get(host + `/pet/${id}`).then(response => {
+                expect(response.data).to.not.include({ id });
+            });            
         } catch (err) {
-            if (err.hasOwnProperty('response')) {  // 404 response, check if pet is deleted
-                expect(err.response.data).to.include({ message: 'Pet not found' })
-            } else {  // pet not deleted, throwing err out for reporting
+            if (err.hasOwnProperty('response')) {  
+                // 404 response, check if pet is deleted
+                expect(err.response.data).to.include({ message: 'Pet not found' });
+            } else {  
+                // pet not deleted, throwing err out for reporting
                 throw err
             }
         }
     }
     ).timeout(10000)
-
-
 }
 )
